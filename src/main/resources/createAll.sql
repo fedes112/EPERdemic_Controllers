cc4227796fac3335514242060edd5794626fd2ac
Ivan Dominikow <Dominikowivan@gmail.com>
2020-04-24 16:10:57 -0300

    cleanup

--- src/main/resources/createAll.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS patogeno (
-  id int auto_increment NOT NULL ,
-  tipo VARCHAR(255) NOT NULL UNIQUE,
-  cantidadDeEspecies int NOT NULL,
-  PRIMARY KEY (id)
-)
-ENGINE = InnoDB;
